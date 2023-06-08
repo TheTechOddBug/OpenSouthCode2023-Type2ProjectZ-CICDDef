fa6b26dd67a9adef12fd4434e843656ed18632c3
José María Gutiérrez TheTechOddBug <TheTechOddBug@gmail.com>
2023-06-09 00:53:53 +0200

    Repo url setup.

--- vars/projectCICDDef.groovy
+++ vars/projectCICDDef.groovy
@@ -5,11 +5,13 @@ def getProjectInfo() {
   def projectCICDDef = [:]
   def theProjectPipelineStagesDef = [:]
 
+  def theProjectRepo = 'https://github.com/TheTechOddBug/OpenSouthCode2023-Type2ProjectZ.git'
   def theProjectType = 'projectType2'
 
   theProjectPipelineStagesDef.put(10, ['ProjectZ - Stage 1 - OvWr', { println 'Overwriting stageOne in ProjectZ.' }])
   theProjectPipelineStagesDef.put(15, ['ProjectZ - New Stage 1.5', { println 'New pipeline stage by ProjectZs.' }])
 
+  projectCICDDef.projectRepo = theProjectRepo
   projectCICDDef.projectType = theProjectType
   projectCICDDef.projectPipelineStagesDef = theProjectPipelineStagesDef
 
